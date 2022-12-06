bf10907a620bfaedb024a1fa4db5ed6c459c4f75
maria-studiodialect <maria@studiodialect.com>
2022-12-06 12:25:26 +0000

    btn

--- indexBrowserified.js
+++ indexBrowserified.js
@@ -9,7 +9,7 @@ const player = new Player(SHARED_LINK, 'furioos_container', {
   whiteLabel: true,
   hideTitle: true,
   hideToolbar: false,
-  hidePlayButton: false,
+  hidePlayButton: true,
   debugAppMode: false,
 });
 /*
